8eb0fe630cbd9e5d9bc28a89029313942112e4f9
Luffy1087 <francesco.1987@alice.it>
2019-10-23 22:30:22 +0200

    fix view

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _formControls = require('./dist/formControls.js');
-
-var _formControls2 = _interopRequireDefault(_formControls);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
