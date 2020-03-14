0e7b944b8b53e7674abe106f5f4f6135e1b84718
Francesco <francesco.1987@alice.it>
2020-03-14 19:12:33 +0100

    Refactor Index.js/Server.js

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ import './lib/webSocket';
 import { server } from './lib/server';
 import { createWindow, app } from './lib/windows';
 
-const startProgram = () => {
-    const window = createWindow('index', './dist/index.html');
-    
-    window.on('closed', () => server.close);
-}
 
-app.on('ready', startProgram);
+(function(Program) {
+    new Program();
+})(class Program {
+    constructor() {
+        app.on('ready', this.startProgram);
+    }
+
+    startProgram() {
+        const window = createWindow('index', './dist/index.html');
+        window.on('closed', () => server.close());
+    }
+});
